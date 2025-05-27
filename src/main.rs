815a6b5a9c4ba6b4edb535676c46673475c9db67
le.toille <125640909+letoille@users.noreply.github.com>
2025-05-27 15:04:13 +0800

    log init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use tracing::Level;
+use tracing::{info, level_filters::LevelFilter};
+use tracing_appender;
+use tracing_subscriber::filter::Targets;
+use tracing_subscriber::{Layer, Registry, fmt, layer::SubscriberExt, util::SubscriberInitExt};
+
 fn main() {
-    println!("Hello, world!");
+    let filter = Targets::new()
+        .with_target("xspb", Level::INFO)
+        .with_target("application", Level::INFO)
+        .with_target("edge", Level::INFO);
+
+    let file_appender = tracing_appender::rolling::daily("logs", "xspb.log");
+    let (nb, _guard) = tracing_appender::non_blocking(file_appender);
+    let file_layer = fmt::layer()
+        .with_ansi(false)
+        .with_writer(nb)
+        .with_filter(filter.clone())
+        .with_filter(LevelFilter::INFO);
+
+    let stdout_layer = fmt::layer()
+        .with_writer(std::io::stdout)
+        .with_filter(filter.clone())
+        .with_filter(LevelFilter::INFO);
+
+    Registry::default()
+        .with(stdout_layer)
+        .with(file_layer)
+        .init();
+
+    info!("Hello, xspb!");
 }
